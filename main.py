3f34e04d25e2ed8b6ac465c7d7e27fb2b7dfa1a1
19522531 <19522531@gm.uit.edu.vn>
2022-05-09 09:46:36 +0700

    add

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ast import Constant
 from xml.dom.minidom import Element
 from click import option
 from selenium import webdriver
@@ -7,43 +8,39 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 from datetime import datetime
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+#driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-
-
+sleep_long = 30
+sleep_short = 10
 def get_list_comments_by_link(link):
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get(link)
     
-    time.sleep(5)    
+    time.sleep(sleep_long)    
 
     ### button language click
     check_lang =  btn = driver.find_elements_by_xpath("//span[contains(text(), 'More languages')]")
+    time.sleep(sleep_short)
     check_lang[0].click()
-    time.sleep(10)
     
     ###  button vietnamese click to select vietnamese
     try:
         btn_vi = driver.find_element_by_id("filters_detail_language_filterLang_vi")
-        # btn_vi = driver.find_element_by_xpath("//span[contrains(text(),'Vietnamese')]")
-        # time.wait(3)
-        time.sleep(30)
+        time.sleep(sleep_long)
         btn_vi.click()
-        time.sleep(3)
+        time.sleep(sleep_short)
     except:
         print("No Vietnamese")
         driver.close()
-        driver.quit()
         return
     
 
 
-    time.sleep(5)
-
     btn_showmore = driver.find_element_by_class_name("ulBlueLinks")
-    time.sleep(20)
+    time.sleep(sleep_long)
     btn_showmore.click()
 
-    time.sleep(3)
+    time.sleep(sleep_short)
     list_comment = driver.find_elements_by_class_name("partial_entry")
     lst = []
     for comment in list_comment:
@@ -56,22 +53,14 @@ def get_list_comments_by_link(link):
     with open("./Data/" + current_time+ ".txt", "w", encoding="utf8") as file:
         file.write("\n\n".join(lst))
     driver.close()
-    driver.quit()
 
-    # next_btn = driver.find_elements_by_xpath("//a[contains(text(), 'Next')]")
-    # if len(next_btn)>=2:
-    #     link  = next_btn[0].get_attribute("href")
-    #     print("replace link\n\n")
-    #     print(link)
-    # else: 
-    #     print("Not replace\n\n")  
-
-# with open("list_link.txt" , "r", encoding="utf8") as file:
-#     list_link = file.read().split("\n")
-# for link in list_link:
-#     try:
-#         get_list_comments_by_link(link)
-#     except:
-#         continue
-link = "https://www.tripadvisor.com//Restaurant_Review-g293925-d5565387-Reviews-Hard_Rock_Cafe-Ho_Chi_Minh_City.html"
-get_list_comments_by_link(link)
+def extra_comment(path):
+    with open(path , "r", encoding="utf8") as file:
+        list_link = file.read().split("\n")
+    for link in list_link:
+        try:
+            get_list_comments_by_link(link)
+        except:
+            continue
+path = "list_link.txt"
+extra_comment(path)
